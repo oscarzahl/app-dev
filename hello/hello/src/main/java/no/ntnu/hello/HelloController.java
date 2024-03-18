aad8572205b02b1ac8ebbcbb5cd182ed84349163
oscarzahl <oscaroz@stud.ntnu.no>
2024-03-18 15:02:08 +0100

    commit

---
+++ hello/hello/src/main/java/no/ntnu/hello/HelloController.java
@@ -0,0 +1,16 @@
+package no.ntnu.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String greeting() {
+        return "Hello, World!";
+    }
+    
+    
+}
